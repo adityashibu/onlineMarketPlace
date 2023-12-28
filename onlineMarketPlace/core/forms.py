da50fa48c6bc3b3af6ad15abf40812bcdae0c455
Aditya Shibu <as2397@hw.ac.uk>
2023-12-28 19:11:56 +0400

    Added logic for letting users sign up

--- onlineMarketPlace/core/forms.py
+++ onlineMarketPlace/core/forms.py
@@ -6,3 +6,20 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User 
         fields = ('username', 'email', 'password1', 'password2')
+        
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your Username',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your E-Mail ID',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter your password',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm your password',
+        'class': 'w-full py-4 px-6 rounded-xl',
+    }))
