e9e395838e57c565a0c97fb12e4d0cef0a5dabbd
Aditya Shibu <as2397@hw.ac.uk>
2024-01-16 20:12:51 +0400

    Added style to add new items

--- onlineMarketPlace/item/forms.py
+++ onlineMarketPlace/item/forms.py
@@ -6,3 +6,13 @@ class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item 
         fields = ('category', 'name', 'description', 'price', 'image',)
+        
+        widgets = {
+            'category': forms.Select(attrs= {
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            
+            'name': forms.TextInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+        }
